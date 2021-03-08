b5cb2a118a1219bf8cdedf99e06e537132060f17
Roberto Nascimento <robertonscjr@gmail.com>
2021-03-08 18:23:52 -0300

    Update control plane script

--- bootstrap/4-deploy_control_plane.sh
+++ bootstrap/4-deploy_control_plane.sh
@@ -1,5 +1,7 @@
 kubeadm init
 
+kubectl apply -f https://docs.projectcalico.org/manifests/calico.yaml
+
 sleep 60
 
 mkdir -p /home/ubuntu/.kube
@@ -7,4 +9,3 @@ sudo cp -i /etc/kubernetes/admin.conf /home/ubuntu/.kube/config
 sudo chown ubuntu:ubuntu /home/ubuntu/.kube/config
 sudo chown ubuntu:ubuntu /home/ubuntu/.kube
 
-kubectl apply -f https://docs.projectcalico.org/manifests/calico.yaml
