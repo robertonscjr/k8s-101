5fbf7a65f0b881a96e1359797a22e1e922da40a1
Roberto Nascimento <robertonscjr@gmail.com>
2021-03-10 13:20:46 -0300

    Update control plane deployment

--- bootstrap/4-deploy_control_plane.sh
+++ bootstrap/4-deploy_control_plane.sh
@@ -3,6 +3,6 @@ kubeadm init
 sleep 60
 
 mkdir -p /home/ubuntu/.kube
-sudo cp -i /etc/kubernetes/admin.conf /home/ubuntu/.kube/config
+sudo cp -f /etc/kubernetes/admin.conf /home/ubuntu/.kube/config
 sudo chown ubuntu:ubuntu /home/ubuntu/.kube/config
 sudo chown ubuntu:ubuntu /home/ubuntu/.kube
